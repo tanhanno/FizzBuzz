f44dabd1d10082e7b210a9a6a19ae8f78624bb10
Tanja Hannonen <87722561+tanhanno@users.noreply.github.com>
2024-04-25 10:11:29 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ var count = 1;
 var result = document.getElementById("result");
 var run = document.getElementById("run")
 
-run.addEventListener("click", function() {
-    for (let i = 0 ; i < 100; i++) {
+function fizzBuzz() {
+
+    setTimeout(() => {
+        for (let i = 0 ; i < 100; i++) {
         if (count % 3 === 0 && count % 5 === 0) {
             output.push("FizzBuzz")
         } else if (count % 3 === 0 ) {
@@ -14,9 +16,16 @@ run.addEventListener("click", function() {
         } else {
             output.push(count);
         }
-        count++;
+        
         console.log(output);
-        result.textContent = output.join("\, ");
+        result.textContent = output.join(", ");
+        count++;
     }
+    }, 500)
+   
+}
+
+run.addEventListener("click", function() {
     run.disabled = true;
+    fizzBuzz();
 })
